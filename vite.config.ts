a96dc4a4af41f004c3ff4379b80d2ad9f4c24269
Mazhorova <mazhorova1987@mail.ru>
2024-01-26 21:13:12 +0300

    2

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
+import { resolve } from 'path'
+
 export default {
     base: "/vite-new/",
+    build: {
+      rollupOptions: {
+        input: {
+          // @ts-ignore
+          main: resolve(__dirname, 'index.html'),
+          // @ts-ignore
+          a1: resolve(__dirname, 'a1.html'),
+          // @ts-ignore
+          a2: resolve(__dirname, 'a2.html'),
+        }
+      }
+    }
   }
